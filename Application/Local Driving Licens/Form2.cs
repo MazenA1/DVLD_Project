735fe6841569f75fb8d2c5bc16f70ff39d45e7ca
MazenA11 <mazenalazhr2022@gmail.com>
2025-10-21 22:23:12 +0300

    added new form 2

---
+++ Application/Local Driving Licens/Form2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVLD_Project.Application.Local_Driving_Licens
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+    }
+}
